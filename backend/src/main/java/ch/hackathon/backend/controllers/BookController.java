c36a3248fe12fe507ccd06084bb9bbba4d0dfa5e
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-10-05 13:35:47 +0200

    remove unused imports

--- backend/src/main/java/ch/hackathon/backend/controllers/BookController.java
+++ backend/src/main/java/ch/hackathon/backend/controllers/BookController.java
@@ -3,8 +3,6 @@ package ch.hackathon.backend.controllers;
 import ch.hackathon.backend.models.Book;
 import ch.hackathon.backend.services.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
