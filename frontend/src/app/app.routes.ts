7c005d93736dace1bd9cc0fcfd548f0441b3350d
Orian <lol.orian2010@gmail.com>
2024-10-11 23:57:51 +0200

    init router,lecture

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LectureComponent } from './lecture/lecture.component';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    component: DashboardComponent,
+    title: 'Dashboard'
+  },
+  {
+    path: 'lecture',
+    component: LectureComponent
+  }
 ];
---
+++ frontend/src/app/lecture/lecture.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lecture',
+  standalone: true,
+  imports: [],
+  templateUrl: './lecture.component.html',
+  styleUrl: './lecture.component.css'
+})
+export class LectureComponent {
+
+}
