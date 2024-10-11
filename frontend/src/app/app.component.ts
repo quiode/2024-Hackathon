bb37fc8f2a14936f05030350ded7df75560f5d09
Orian <lol.orian2010@gmail.com>
2024-10-11 22:29:14 +0200

    header to app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashbordComponent } from "./dashbord/dashbord.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashbordComponent],
+  imports: [RouterOutlet, DashbordComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
