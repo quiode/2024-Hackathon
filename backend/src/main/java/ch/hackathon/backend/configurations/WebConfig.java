3f8b425b333061c60dd63e9f327cf705ea7ee301
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-10-12 00:25:17 +0200

    update web config error

--- backend/src/main/java/ch/hackathon/backend/configurations/WebConfig.java
+++ backend/src/main/java/ch/hackathon/backend/configurations/WebConfig.java
@@ -2,11 +2,11 @@ package ch.hackathon.backend.configurations;
 
 import ch.hackathon.backend.interceptors.UserInterceptor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Component
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final UserInterceptor userInterceptor;
