1d3fb6647641bf1947122a06595316ac3716fa96
Orian <lol.orian2010@gmail.com>
2024-10-12 12:35:25 +0200

    game router import

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { negateAuthGuard } from './shared/guards/negate-auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LectureComponent } from './pages/lecture/lecture.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { GameComponent } from './pages/game/game/game.component';
 
 export const routes: Routes = [
   {
