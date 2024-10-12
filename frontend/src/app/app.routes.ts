714cdd30d3f815010027bc238db951027795cb85
Orian <lol.orian2010@gmail.com>
2024-10-12 12:36:11 +0200

    fixed game folder structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { negateAuthGuard } from './shared/guards/negate-auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LectureComponent } from './pages/lecture/lecture.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { GameComponent } from './pages/game/game/game.component';
+import { GameComponent } from './pages/game/game.component';
 
 export const routes: Routes = [
   {
