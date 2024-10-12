bb33fa13f4fdfe35894c29f9eb65e7aed39e8737
DESKTOP-FPLJ9KU\Baumgarten <baumgarten.benedikt@icloud.com>
2024-10-12 10:39:40 +0200

    Fixed Skill Issue - probably not

--- backend/src/main/java/ch/hackathon/backend/repositories/CardRepository.java
+++ backend/src/main/java/ch/hackathon/backend/repositories/CardRepository.java
@@ -1,4 +1,7 @@
 package ch.hackathon.backend.repositories;
 
-public interface CardRepository {
+import ch.hackathon.backend.models.Card;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CardRepository extends CrudRepository<Card, Long> {
 }
