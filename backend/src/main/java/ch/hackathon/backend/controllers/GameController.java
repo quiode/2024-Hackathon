f080bbc5acae0fcd66529add208a0b4edc45f8a5
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-10-12 22:41:52 +0200

    Add game creation in controller

--- backend/src/main/java/ch/hackathon/backend/controllers/GameController.java
+++ backend/src/main/java/ch/hackathon/backend/controllers/GameController.java
@@ -1,15 +1,45 @@
 package ch.hackathon.backend.controllers;
 
+import ch.hackathon.backend.models.Game;
+import ch.hackathon.backend.models.Lecture;
+import ch.hackathon.backend.models.LectureTimeframe;
+import ch.hackathon.backend.models.User;
+import ch.hackathon.backend.repositories.LectureTimeframeRepository;
 import ch.hackathon.backend.services.GameService;
+import ch.hackathon.backend.services.LectureService;
 import lombok.RequiredArgsConstructor;
+
+import java.net.HttpCookie;
+import java.time.Instant;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/game")
 @RequiredArgsConstructor
 public class GameController {
   private final GameService gameService;
+  private final LectureService lectureService;
 
+  @PostMapping("/lecture/{id}/current")
+  public Game createGame(@RequestAttribute User user, @PathVariable Long id) {
+    Optional<Lecture> lectureRes = lectureService.getById(id);
+    Lecture lecture = lectureRes.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "No lecture associated with id"));
+
+    Optional<Game> currentGame = gameService.getCurrentGameForLecture(lecture); 
+    if (currentGame.isPresent())
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "");
+
+    LectureTimeframe currentTF = lectureService.getTimeframeForLecture(lecture, Instant.now())
+      .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "No current timeframe for lecture"));
+
+    // FIXME: actually let the user/timeframe decide which professor is currently holding the lecture
+    return gameService.createGame(lecture, lecture.getProfessors().stream().findAny().get(), currentTF);
+    
+  }
 
 
 }
--- backend/src/main/java/ch/hackathon/backend/services/GameService.java
+++ backend/src/main/java/ch/hackathon/backend/services/GameService.java
@@ -1,37 +1,85 @@
 package ch.hackathon.backend.services;
 
+import ch.hackathon.backend.models.Card;
 import ch.hackathon.backend.models.Game;
 import ch.hackathon.backend.models.Lecture;
 import ch.hackathon.backend.models.LectureTimeframe;
 import ch.hackathon.backend.models.Professor;
 import ch.hackathon.backend.models.User;
+import ch.hackathon.backend.repositories.CardRepository;
 import ch.hackathon.backend.repositories.GameRepository;
+import ch.hackathon.backend.repositories.LectureRepository;
 import ch.hackathon.backend.repositories.LectureTimeframeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 
+
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class GameService {
+    private static final int BINGO_SIZE = 4;
     private final GameRepository gameRepository;
     private final LectureTimeframeRepository lectureTimeframeRepository;
+    private final CardRepository cardRepository;
+    private final LectureRepository lectureRepository;
+
 
 
     public Game createGame(Lecture lecture, Professor professor, LectureTimeframe timeframe) {
-        Game game = new Game(null, lecture, professor, timeframe, new HashSet<>(), new HashSet<>(), -1, -1);
+        long cardCount = cardRepository.count();
+        if (cardCount == 0)
+            return null;
+        if (cardCount < BINGO_SIZE * BINGO_SIZE)
+            log.warn("Not enough cards ({}) to fill Bingo({}x{}), using duplicates.", cardCount, BINGO_SIZE, BINGO_SIZE);
+        // FIXME: this is terribly inefficient
+        List<Card> cards = cardRepository.findAll();
+        cards.sort(new Comparator<Card>() {
+            public int compare(Card c1, Card c2) {
+                int voteDeltaC1 = c1.getUpvotes().size() - c1.getDownvotes().size();
+                int voteDeltaC2 = c2.getUpvotes().size() - c2.getDownvotes().size();
+                return voteDeltaC2 - voteDeltaC1;
+            }
+        });
+        List<Card> cardMatrix = cards.stream().limit(BINGO_SIZE * BINGO_SIZE).collect(Collectors.toCollection(ArrayList::new));
+        while (cardMatrix.size() < BINGO_SIZE * BINGO_SIZE) {
+            cardMatrix.add(cards.getFirst());
+        }
+
+        
+        Game game = new Game(null, lecture, professor, timeframe, new HashSet<>(), new HashSet<>(cardMatrix), BINGO_SIZE, BINGO_SIZE);
         return gameRepository.save(game);
     }
 
     public Optional<Game> getGame(User user, long lecture_id) {
         return gameRepository.findById(lecture_id);
+    }
+
+    public Optional<Game> getCurrentGameForLecture(Lecture lecture) {
+        Instant now = Instant.now();
+        List<LectureTimeframe> potentialLectures = lectureTimeframeRepository.findAllByStartDateBeforeAndEndDateAfter(now, now);
+        Optional<LectureTimeframe> currentTimeframe = potentialLectures.stream()
+            .filter(tf -> {
+                Optional<Lecture> l = lectureRepository.findByDates(tf);
+                return l.isPresent() && l.get().getId().equals(lecture.getId());
+            })
+            .findFirst();
+        if (currentTimeframe.isEmpty()) return Optional.empty();
+        Optional<Game> game = gameRepository.findByLectureTimeframeAndLecture(currentTimeframe.get(), lecture);
+        return game;
     }
 
     public Optional<Game> getCurrentGameForUser(User user) {
@@ -40,7 +88,7 @@ public class GameService {
         for (var tf: potentialTimeframes) {
             List<Game> potentialGames = gameRepository.findAllByLectureTimeframe(tf);
             for (Game game: potentialGames) {
-                if (game.getUsers().stream().anyMatch(u -> u.getId() == user.getId())) {
+                if (game.getUsers().stream().anyMatch(u -> u.getId().equals(user.getId()))) {
                     return Optional.of(game);
                 }
             }
