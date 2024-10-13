12f9627eefb8962dbebe80b68a05fdecb8a01eaf
Orian <lol.orian2010@gmail.com>
2024-10-13 11:06:07 +0200

    nichts

--- frontend/src/app/pages/game/board-tile/board-tile.component.ts
+++ frontend/src/app/pages/game/board-tile/board-tile.component.ts
@@ -1,7 +1,8 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, Signal } from '@angular/core';
 import { GameService } from '../../../shared/services/game.service';
 import { Card } from '../../../shared/models/Card';
 import { CommonModule } from '@angular/common';
+import { Bingo } from '../../../shared/models/Bingo';
 
 @Component({
   selector: 'app-board-tile',
@@ -10,13 +11,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './board-tile.component.html',
   styleUrl: './board-tile.component.css'
 })
-export class BoardTileComponent {
+export class BoardTileComponent  {
   @Input() status: number;
   card = input.required<Card>();
   pos = input.required<number>();
 
+  board: Signal<Bingo | undefined>;
+
   constructor(private gameService: GameService) {
     this.status = 0;
+    this.board = gameService.setBoard();
   }
 
   clickCard() {
