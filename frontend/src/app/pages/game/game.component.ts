ee616b58535dd456d4471d36df6d2a8b34422334
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-10-13 10:51:03 +0200

    poll game every 5 secs

--- frontend/src/app/pages/game/game.component.ts
+++ frontend/src/app/pages/game/game.component.ts
@@ -1,9 +1,10 @@
-import { Component, signal, Signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal, Signal } from '@angular/core';
 import { GameService } from '../../shared/services/game.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BoardComponent } from './board/board.component';
 import { Game } from '../../shared/models/Game';
 import { LectureTimeframeComponent } from "../../components/lecture-timeframe/lecture-timeframe.component";
+import { interval, Subscription, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -12,20 +13,31 @@ import { LectureTimeframeComponent } from "../../components/lecture-timeframe/le
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent {
+export class GameComponent implements OnInit, OnDestroy {
   game: Signal<Game | undefined>;
+  updateSubscription?: Subscription;
+  id: number;
 
   constructor(private gameService: GameService, private route: ActivatedRoute, private router: Router) {
     if (route.snapshot.firstChild == null) {
       console.log("was here")
       router.navigate([""]);
       this.game = signal(undefined);
+      this.id = 0;
     }
     else {
-      let id = route.snapshot.firstChild.params['id'];
-      gameService.setGame(id);
+      this.id = route.snapshot.firstChild.params['id'];
+      gameService.setGame(this.id);
       this.game = gameService.getCurrentGame();
     }
+  }
+
+  ngOnInit(): void {
+    this.updateSubscription = interval(5 * 1000).subscribe(_ => this.gameService.setGame(this.id));
+  }
+
+  ngOnDestroy(): void {
+    this.updateSubscription?.unsubscribe();
   }
 
 }
